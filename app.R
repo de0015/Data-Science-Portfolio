30510c4009ccc6ac04a50efc79f573d8aa575297
Dan <31831587+de0015@users.noreply.github.com>
2020-09-13 17:56:38 -0400

    The Social Media Machine

--- app.R
+++ app.R
@@ -1,7 +1,12 @@
-library(RedditExtractoR) # Reddit Search and Extractor
-library(tuber) # Youtube Api 
-library(rvest) # Web scraper
+library(RedditExtractoR) 
+library(tuber) 
+library(rvest) 
 library(shiny)
+library(shinyWidgets)
+library(DT)
+
+Sys.setlocale('LC_ALL','C')
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -9,33 +14,26 @@ ui <- fluidPage(
     # Application title
     titlePanel("Social Media Search Tool"),
     
+
+    ui <- fluidPage(
+      actionButton("go", "Go"),
+      textInput("n", "search"),
+      dataTableOutput("table")
+      
+    ))
     
-
-    sidebarPanel(textInput("term", "Enter a Term"),
-                   numericInput("cant", "Select Number of Returns",1,0,200),
-                   radioButtons("lang", "Select the Language",c(
-                       "English"="en",
-                       "Castellano"="es",
-                       "Deutsch"="de")),
-                   submitButton(text = "Run")
-                   ))
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           h4("Last 5 Tweets"),
-           tableOutput("table"),
-           plotOutput("wordcl"))
-           
-        
-
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-  
-  rawData <- reactive(function(){
-    tweets <- get_reddit(search_terms = input$term, n=input$cant,lang=input$lang)
-  })
-}
-  
-# Run the application 
-shinyApp(ui = ui, server = server)
+    server <- function(input, output) {
+      
+      randomVals <- eventReactive(input$go, {
+        reddit_urls(input$n)
+      })
+      output$table <- DT::renderDataTable({
+        (randomVals())
+      })
+    
+    }
+    
+    
+    shinyApp(ui, server)
+    
+    #Not sure why but it works...
